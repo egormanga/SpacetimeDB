c3834767dd29359ead627ccd07be7930eaf7bce3
Egor Vorontsov <sdoregor@sdore.me>
2025-11-08 02:54:59 +0300

    Implemented Some-typed literals in SQL.

--- crates/expr/src/lib.rs
+++ crates/expr/src/lib.rs
@@ -296,6 +296,9 @@ pub(crate) fn parse(value: &str, ty: &AlgebraicType) -> anyhow::Result<Algebraic
             .map(AlgebraicValue::from)
             .map_err(|err| anyhow!(err))
     };
+    let to_option = || {
+        Ok(AlgebraicValue::OptionSome(parse(value, ty.as_option().unwrap()).map_err(|_| InvalidLiteral::new(value.to_string(), ty))?))
+    };
     let to_simple_enum = || {
         Ok(AlgebraicValue::enum_simple(
             match ty {
@@ -425,6 +428,7 @@ pub(crate) fn parse(value: &str, ty: &AlgebraicType) -> anyhow::Result<Algebraic
         t if t.is_identity() => to_identity(),
         t if t.is_connection_id() => to_connection_id(),
         t if t.is_uuid() => to_uuid(),
+        t if t.is_option() => to_option(),
         t if t.is_simple_enum() => to_simple_enum(),
         t => bail!("Literal values for type {} are not supported", fmt_algebraic_type(t)),
     }
